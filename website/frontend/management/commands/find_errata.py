e229082a0c229c4eee0e1e847cfa59202dc8dfa2
fin <fin+github@xbhd.org>
2015-08-02 16:31:56 +0200

    create json files for each day

--- website/frontend/management/commands/find_errata.py
+++ website/frontend/management/commands/find_errata.py
@@ -1,9 +1,16 @@
+import os
+import re
+import json
+import datetime
+import itertools
+import operator
+
 from django.core.management.base import BaseCommand
 from website.frontend.models import Article, Version
 from django.db.models.aggregates import Count
 from django.conf import settings
 
-import re
+from optparse import make_option
 
 import diff_match_patch
 
@@ -12,17 +19,17 @@ def has_erratum(old, new):
     dmp.Diff_Timeout = 3 # seconds; default of 1 is too little
     diff = dmp.diff_main(old, new)
     dmp.diff_cleanupSemantic(diff)
-    chars_added   = '\n'.join([text for (sign, text) in diff if sign == 1]).lower()
+    chars_added   = '\n'.join([text for (sign, text) in diff if sign == 1])
     is_update = False
     for signifier in settings.CORRECTION_SIGNIFIERS:
-        match = re.search(signifier, chars_added, re.MULTILINE)
+        match = re.search(signifier, chars_added, re.IGNORECASE|re.MULTILINE)
         if match:
             is_update = match
     if is_update:
         remaining_chars = chars_added[is_update.start():]
         l = is_update.end()-is_update.start()
         remaining_lines = remaining_chars.split('\n')
-        if len(remaining_lines[0])>l*5:
+        if len(remaining_lines[0])>l*5: # just one paragraph, or multiple?
             return remaining_lines[0]
         else:
             return remaining_chars
@@ -30,15 +37,51 @@ def has_erratum(old, new):
         return False
 
 class Command(BaseCommand):
-    help = ''' do the thing '''
+    help = ''' (Re)create JSON files for every day's changes. '''
+    option_list = BaseCommand.option_list + (
+        make_option('--all',
+            action='store_true',
+            default=False,
+            help='(Re)create all diff jsons, not only today\'s'),
+        )
 
     def handle(self, *args, **options):
-        versioned_articles = Article.objects.all()\
-            .annotate(count_versions=Count('version'))\
-            .filter(count_versions__gt=1)
-        for versioned_article in versioned_articles:
-            versions = versioned_article.versions()
-            for pair in zip(versions[0:len(versions)-1], versions[1:]):
+        do_all = options['all']
+
+        versions = Version.objects.all()
+
+        if not do_all:
+            versions = versions.filter(date__gte=datetime.date.today()-datetime.timedelta(days=1))
+
+        versions = versions.order_by('date')
+
+        versions_by_article = itertools.groupby(versions,
+                key=lambda version: version.article_id)
+
+        errata = []
+        for article_id, versions_since_yesterday in versions_by_article:
+            versions_since_yesterday = list(versions_since_yesterday)
+            versioned_article = Article.objects.get(id=article_id)
+            versions_ever = versioned_article.versions()
+            versions_cur = versions_ever.filter(date__gte=versions_since_yesterday[0].date)
+            if versions_cur.count()<2:
+                continue
+            for pair in zip(versions_cur[0:len(versions_cur)-1], versions_cur[1:]):
                err = has_erratum(pair[0].text(), pair[1].text())
                if err:
-                   print err
+                   errata.append((versioned_article, pair, err,))
+
+        errata_by_date = itertools.groupby(errata,
+                key=lambda (a,p,e,): p[1].date.date()
+        )
+        for date, errata_on_day in errata_by_date:
+            f = open(os.path.join(settings.WEBAPP_ROOT, '..', 'errata', date.isoformat()+'.json'), 'w')
+            towrite = [{'url': versioned_article.url,
+                       'time': pair[1].date.isoformat(),
+                       'update': err,
+                       'article_id': versioned_article.id,} for (versioned_article, pair, err,) in errata_on_day
+                       ]
+            print towrite
+            json.dump(towrite, f)
+            f.close()
+
